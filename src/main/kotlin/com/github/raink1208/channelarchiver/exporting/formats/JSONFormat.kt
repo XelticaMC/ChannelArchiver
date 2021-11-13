99d2570a778fd4623f53051d3668fa37f957b208
raink1208 <kkouga1208@gmail.com>
2021-11-13 10:25:04 +0900

    add: JSONフォーマットを追加

--- src/main/kotlin/com/github/raink1208/channelarchiver/exporting/formats/JSONFormat.kt
+++ src/main/kotlin/com/github/raink1208/channelarchiver/exporting/formats/JSONFormat.kt
@@ -1,12 +1,44 @@
 package com.github.raink1208.channelarchiver.exporting.formats
 
+import com.github.raink1208.channelarchiver.models.AuthorJSONModel
+import com.github.raink1208.channelarchiver.models.MessageJSONModel
+import com.google.gson.Gson
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import net.dv8tion.jda.api.entities.MessageChannel
+import net.dv8tion.jda.api.entities.MessageHistory
 import java.io.File
 
 class JSONFormat : ExportFormat {
     override val extension: String = ".json"
     override suspend fun execute(channel: MessageChannel): File {
         val file = createLogFile()
-        TODO("Not yet implemented")
+        val history = MessageHistory(channel)
+        val list = mutableListOf<MessageJSONModel>()
+        withContext(Dispatchers.IO) {
+            while (true) {
+                val retrieve = history.retrievePast(100).complete()
+                if (retrieve.isEmpty()) break
+            }
+
+            val messages = history.retrievedHistory.reversed()
+
+            for (message in messages) {
+                val messageModel = MessageJSONModel()
+                val authorModel = AuthorJSONModel()
+                messageModel.id = message.id
+                messageModel.content = message.contentRaw
+                messageModel.channelId = message.channel.id
+
+                authorModel.username = message.author.name
+                authorModel.id = message.author.id
+
+                messageModel.author = authorModel
+
+                list.add(messageModel)
+            }
+            file.writeText(Gson().toJson(list))
+        }
+        return file
     }
 }
