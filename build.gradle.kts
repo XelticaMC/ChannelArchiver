219d615175877a6006f91f4ec634909b08c50feb
raink1208 <58714901+raink1208@users.noreply.github.com>
2022-01-07 17:34:34 +0900

    update: build scriptの更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,16 +18,16 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-native-mt")
 
-    implementation("net.dv8tion:JDA:4.3.0_277")
-    implementation("org.yaml:snakeyaml:1.12")
-    implementation("com.google.code.gson:gson:2.8.5")
+    implementation("net.dv8tion:JDA:5.0.0-alpha.3")
+    implementation("org.yaml:snakeyaml:1.29")
+    implementation("com.google.code.gson:gson:2.8.9")
 
-    implementation("ch.qos.logback:logback-classic:1.2.3")
+    implementation("ch.qos.logback:logback-classic:1.2.10")
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 }
 
 
