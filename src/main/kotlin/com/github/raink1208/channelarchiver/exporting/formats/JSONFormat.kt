1403fdc23c820980d060ab171917b24a2cab40f7
raink1208 <58714901+raink1208@users.noreply.github.com>
2022-02-08 12:40:40 +0900

    リファクタリング

--- src/main/kotlin/com/github/raink1208/channelarchiver/exporting/formats/JSONFormat.kt
+++ src/main/kotlin/com/github/raink1208/channelarchiver/exporting/formats/JSONFormat.kt
@@ -9,7 +9,7 @@ import net.dv8tion.jda.api.entities.MessageChannel
 import net.dv8tion.jda.api.entities.MessageHistory
 import java.io.File
 
-class JSONFormat : ExportFormat {
+class JSONFormat: ExportFormat {
     override val extension: String = ".json"
     override suspend fun execute(channel: MessageChannel): File {
         val file = createLogFile()
